0fc4bf270c00acd964505cdc2def2e94f8fdbba0
Deepak Shkaya <deepakshakya4455@gmail.com>
2020-06-28 16:05:49 +0530

    Updates

---
+++ precache-manifest.d98d943736887154c92ecf399df5a5dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3855a7e09f82c21ed3280d2a82d2c8e8",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "af278a5f6cbf2e2722d0",
+    "url": "/React-ToDo-App/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "86b8edc0d1e3eb7c5e8e",
+    "url": "/React-ToDo-App/static/css/main.3f36d1d4.chunk.css"
+  },
+  {
+    "revision": "af278a5f6cbf2e2722d0",
+    "url": "/React-ToDo-App/static/js/2.2f9524a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-ToDo-App/static/js/2.2f9524a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86b8edc0d1e3eb7c5e8e",
+    "url": "/React-ToDo-App/static/js/main.a3ac3d05.chunk.js"
+  },
+  {
+    "revision": "142b1db82957e261f186",
+    "url": "/React-ToDo-App/static/js/runtime-main.7fd315bc.js"
+  }
+]);
